298517a65f61d8fb364b1679573164fe6a2744df
josephruse <joseph.ruse@nhs.net>
2025-09-09 11:55:47 +0100

    fix

--- my_package/tests/test_add.py
+++ my_package/tests/test_add.py
@@ -2,8 +2,3 @@ from app_joeruse import app_joeruse
 
 def test_add():
     assert app_joeruse.add(1,1) == 2
-name: Install dependencies
-  run: |
-    python -m pip install --upgrade pip
-    pip install pytest pytest-cov
-    pytest --doctest-modules --junitxml=junit/test-results.xml --cov=com --cov-report=xml --cov-report=html
